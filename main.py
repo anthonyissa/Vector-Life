36cb1b34b56dae7950162263b32b34e93d9452ae
Anthony ISSA <anthony.issa@hotmail.fr>
2021-12-25 19:20:17 +0100

    point impact done

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+vecteur = np.array([[1],
+                    [0]])
+origine = np.array([[-2],
+                    [4]])
+
+droite = np.array([[1, 5],
+                    [1, 4]])
+
+def calculT(origineVecteur, vecteur, droite):
+    droiteAS = np.array([[droite[0][0], origineVecteur[0][0]],
+                   [droite[1][0], origineVecteur[1][0]]])
+    vecteurAS = vDirecteur(droiteAS)
+    normalAB = vNormal(vDirecteur(droite))
+    return (-np.dot(vecteurAS, np.transpose(normalAB))[0][0]/(np.dot(np.transpose(normalAB), vecteur)[0][0]))
+
+def toucheLaDroite(origineVecteur, vecteur, droite):
+    t = calculT(origineVecteur, vecteur, droite)
+    if t < 0:
+        return False
+    x = origineVecteur[0][0] + t * vecteur[0][0]
+    y = origineVecteur[1][0] + t * vecteur[1][0]
+    return (x <= max(droite[0, :][0], droite[0, :][1]) and x >= min(droite[0, :][0], droite[0, :][1])
+    and y <= max(droite[1, :][0], droite[1, :][1]) and y >= min(droite[1, :][0], droite[1, :][1]))
+
+def vNormal(vecteur):
+    return np.array([[-vecteur[1, :][0]],
+                     [vecteur[0, :][0]]])
+
+def vDirecteur(droite):
+    return np.array([[droite[:, 1][0]-droite[:, 0][0]],
+                     [droite[:, 1][1]-droite[:, 0][1]]])
+
+def construireVecteur(x, y):
+    return np.array([[x],
+                    [y]])
+
+def construireDroite(x1, y1, x2, y2):
+    return np.array([[x1, x2],
+                       [y1, y2]])
+
+
+print(toucheLaDroite(construireVecteur(0, 1), construireVecteur(1, 0.5), construireDroite(2, 2, 2, 0)))
