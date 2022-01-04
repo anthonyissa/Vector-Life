fa6f7e1345c85be3e217b8cb5484cf4c5503ea12
DN-Pixel <david.94190@hotmail.fr>
2022-01-04 06:54:30 +0100

    last puh

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ import numpy as np
 import matplotlib.lines as mlines
 from matplotlib.animation import FuncAnimation
 
-vecteur = np.array([[1],
-                    [-0.5]])
-origine = np.array([[-2],
-                    [4]])
-
-droite = np.array([[1, 1],
-                    [0, 4]])
+vecteur = np.array([[1],[-0.5]])
+origine = np.array([[0],[9]])
+droite = np.array([[-2,10 ],[-2, 10]])
 
 translation = None
 
@@ -30,27 +26,33 @@ def toucheLaDroite(origineVecteur, vecteur, droite):
         return None
     x = origineVecteur[0][0] + t * vecteur[0][0]
     y = origineVecteur[1][0] + t * vecteur[1][0]
-    print(f"Point d'intersection: {x}, {y}")
+
     if (x <= max(droite[0, :][0], droite[0, :][1]) and x >= min(droite[0, :][0], droite[0, :][1])
     and y <= max(droite[1, :][0], droite[1, :][1]) and y >= min(droite[1, :][0], droite[1, :][1])):
-        return np.array([[int(x)],
-                         [int(y)]])
+        return np.array([[(x)],
+                         [(y)]])
     else:
         return None
 
 
 # Retourne l'angle du vecteur
 def vecteurEngendre(vecteur, vecteurNormal):
-    if(np.dot(np.transpose(vecteurNormal), vecteur ) < 0):
+    print(vecteur)
+    print(vecteurNormal)
+    print(np.dot(np.transpose(vecteurNormal), vecteur))
+    if(np.dot(np.transpose(vecteurNormal), vecteur ) > 0):
         vecteurNormal = np.array([[-vecteurNormal[0][0]],
                                  [-vecteurNormal[1][0]]])
     hypo = math.sqrt(np.dot(np.transpose(vecteur),vecteur))
-    adjacent = ( (abs(np.dot( np.transpose(vecteurNormal),vecteur)))/math.sqrt(np.dot( np.transpose(vecteurNormal),vecteurNormal)))
-    print(f"Angle: {math.degrees(np.arccos(adjacent / hypo))}")
+    adjacent = ((abs(np.dot( np.transpose(vecteurNormal),vecteur)))/math.sqrt(np.dot( np.transpose(vecteurNormal),vecteurNormal)))
+
+
     return math.degrees(np.arccos(adjacent/hypo))
 
 # Retourne le vecteur après la rotation
 def rotationVecteur(vect, impact, theta):
+    if impact is None :
+        return None
     rotation = theta
     x = vect[0][0]
     y = vect[1][0]
@@ -60,16 +62,18 @@ def rotationVecteur(vect, impact, theta):
 
     boutDuVecteur = impact+vect
 
-    # si pas besoin des coordonnées homogènes
+    #matrice rotation
     mat = np.array([[np.cos(rotation), -np.sin(rotation)],
                     [np.sin(rotation), np.cos(rotation)]])
     premierResultat = np.dot(mat,boutDuVecteur)
     if sontColinéaires(premierResultat, vect):
         mat2 = np.array([[np.cos(-rotation), -np.sin(-rotation)],
                         [np.sin(-rotation), np.cos(-rotation)]])
-        return np.dot(mat2,boutDuVecteur)
+        print("aaaaaaaaaaaaaaaaaaa")
+        return np.dot(mat2,boutDuVecteur),impact
     else:
-        return premierResultat
+
+        return premierResultat,impact
 
 def vNormal(vecteur):
     return np.array([[-vecteur[1, :][0]],
@@ -88,14 +92,59 @@ def construireVecteur(x, y):
                      [y]])
 
 def sontColinéaires(v1, v2):
-    return v1[0][0]*v2[1][0] == v1[1][0]*v2[0][0]
+    return int(v1[0][0])*int(v2[1][0]) == int(v1[1][0])*int(v2[0][0])
+
 
 def construireDroite(x1, y1, x2, y2):
     return np.array([[x1, x2],
                      [y1, y2]])
 
 
-print(rotationVecteur(vecteur, toucheLaDroite(origine, vecteur, droite), vecteurEngendre(vecteur, vNormal(vecteur))))
+
+
+
+
+
+mur1 = construireDroite(0, 0, 0, 8)
+mur2 = construireDroite(0, 10, 10, 10)
+mur3 = construireDroite(10, 10, 10, 2)
+mur4 = construireDroite(10, 0, 0, 0)
+entree = construireDroite(0, 8, 0, 10)
+sortie = construireDroite(10, 0, 10, 2)
+
+listemur = [mur1,mur2,mur3, mur4]
+endCond = [entree,sortie]
+segx1 = construireDroite(4, 9.5, 4, 2)
+segx2 = construireDroite(2.2, 7.27, 3.56, 4.31)
+segx3 = construireDroite(6.96, 5.43, 7.96, 3.21)
+segx4 = construireDroite(3.42, 2.19, 4.94, 0.55)
+
+map = [segx1,segx2,segx3,segx4]
+
+vdir = vDirecteur(construireDroite(0,9,1,8.5))
+# rotationVecteur(vect, impact, theta):
+print(rotationVecteur(vdir, toucheLaDroite(origine, vdir, segx1), vecteurEngendre(vdir, vNormal(vDirecteur(segx1)))))
+def construreSegments (liste) :
+    for mur in liste:
+        x = np.linspace(mur[0, :][0], mur[0, :][1], 2)
+        y = np.linspace(mur[1, :][0], mur[1, :][1], 2)
+        plt.scatter(x, y)
+        plt.plot(x, y)
+
+construreSegments(listemur)
+construreSegments(map)
+
+
+def construireDroite(x1, y1, x2, y2):
+    return np.array([[x1, x2],
+                     [y1, y2]])
+
+plt.quiver(0, 9, 1, -0.5, angles = 'xy', scale_units = 'xy', scale = 1)
+
+
+plt.show()
+
+
 
 
 """
@@ -123,12 +172,6 @@ vec = vDirecteur(droitetest)
 nv = normeVecteur(vec)
 print(nv)
 
-mur1 = construireDroite(0, 0, 0, 8)
-mur2 = construireDroite(0, 0, 0, 8)
-mur3 = construireDroite(0, 0, 0, 8)
-mur4 = construireDroite(0, 0, 0, 8)
-entree = construireDroite(0, 8, 0, 10)
-sortie = construireDroite(10, 0, 10, 2)
 
 
 def visu_point(matPoint, style):
@@ -160,4 +203,3 @@ plt.plot(z)
 plt.show()
 
 """
-
